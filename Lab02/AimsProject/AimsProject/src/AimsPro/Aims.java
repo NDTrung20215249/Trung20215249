3264b477f877c8f3ccba397fe8cfe787e1a4c3ff
NDTRung <130715344+NDTrung20215249@users.noreply.github.com>
2023-04-28 10:43:37 +0700

    Add files via upload

---
+++ Lab02/AimsProject/AimsProject/src/AimsPro/Aims.java
@@ -0,0 +1,19 @@
+package AimsPro;
+
+public class Aims {
+	public static void main(String[] args) {
+		Cart anOrder = new Cart();
+		DVD dvd1 = new DVD("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDVD(dvd1);
+		
+		DVD dvd2 = new DVD("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		anOrder.addDVD(dvd2);
+		DVD dvd3 = new DVD("Aladin", "Animation", 18.99f);
+		anOrder.addDVD(dvd3);
+		System.out.println("Total Cost is: ");
+		System.out.println(anOrder.totalCost());
+		anOrder.removeDVD(dvd3);
+		System.out.println("Total Cost is: ");
+		System.out.println(anOrder.totalCost());
+	}
+}
